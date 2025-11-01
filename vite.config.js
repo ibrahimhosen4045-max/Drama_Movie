776dfdb1378744d4b7811c489b063d88ff46990b
Ibrahim <ibrahimhosen4045@gmail.com>
2025-11-01 15:30:27 +0600

    Fixed vite config and build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/https://github.com/ibrahimhosen4045-max/Drama_Movie.git/',
   plugins: [react(), tailwindcss()],
 })
